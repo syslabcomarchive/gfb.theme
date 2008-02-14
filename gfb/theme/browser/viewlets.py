30e774e08d259bb2d7511023d217c22286a5ecab
Wolfgang Thomas <thomas@syslab.com>
2008-02-14 17:10:42 +0000

    overwrite standard behaviour for personal bar -> don't link username to dashboard

--- gfb/theme/browser/viewlets.py
+++ gfb/theme/browser/viewlets.py
@@ -1,3 +1,6 @@
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from plone.app.layout.viewlets.common import ViewletBase
+from plone.app.layout.viewlets.common import ViewletBase, PersonalBarViewlet
 
+# Overwrite PersonalBarViewlet
+class PersonalBarViewletGFB(PersonalBarViewlet):
+    render = ViewPageTemplateFile('personal_bar_gfb.pt')
