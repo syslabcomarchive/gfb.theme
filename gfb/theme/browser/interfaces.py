f2f6e2d869ffaf1705fcc1d1f27a395e56bfb530
Wolfgang Thomas <thomas@syslab.com>
2008-05-06 17:59:22 +0000

    added a viewlet manager for displaying text below the title on the edit form of a RiskAssessmentLink

--- gfb/theme/browser/interfaces.py
+++ gfb/theme/browser/interfaces.py
@@ -1,5 +1,9 @@
 from plone.theme.interfaces import IDefaultPloneLayer
+from zope.viewlet.interfaces import IViewletManager
 
 class IThemeSpecific(IDefaultPloneLayer):
     """Marker interface that defines a Zope 3 browser layer.
     """
+
+class IGFBEditBelowcontenttitle(IViewletManager):
+    """ ViewletManager for edit forms """
--- gfb/theme/browser/viewlets.py
+++ gfb/theme/browser/viewlets.py
@@ -4,3 +4,7 @@ from plone.app.layout.viewlets.common import ViewletBase, PersonalBarViewlet
 # Overwrite PersonalBarViewlet
 class PersonalBarViewletGFB(PersonalBarViewlet):
     render = ViewPageTemplateFile('personal_bar_gfb.pt')
+
+
+class EditHelp(ViewletBase):
+    render = ViewPageTemplateFile('editing_help.pt')
