b10bb2a08d9e48f54c7998a6bfe7cc55693208f4
Wolfgang Thomas <thomas@syslab.com>
2008-10-21 14:42:58 +0000

    overriding static portlet

---
+++ gfb/theme/portlets/static.py
@@ -0,0 +1,42 @@
+from zope.interface import implements
+from zope.component import getUtility
+
+from plone.portlets.interfaces import IPortletDataProvider
+from plone.portlet.static import static as base
+
+from plone.i18n.normalizer.interfaces import IIDNormalizer
+
+from zope import schema
+from zope.formlib import form
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+from plone.portlet.static import PloneMessageFactory as _
+
+from plone.app.form.widgets.wysiwygwidget import WYSIWYGWidget
+
+class Renderer(base.Renderer):
+    """Portlet renderer.
+
+    This is registered in configure.zcml. The referenced page template is
+    rendered, and the implicit variable 'view' will refer to an instance
+    of this class. Other methods can be added and referenced in the template.
+    """
+
+    render = ViewPageTemplateFile('static.pt')
+
+    @property
+    def available(self):
+        return not self.data.hide
+
+    def css_class(self):
+        """Generate a CSS class from the portlet header
+        """
+        header = self.data.header
+        normalizer = getUtility(IIDNormalizer)
+        return "portlet-static-%s" % normalizer.normalize(header)
+
+    def has_link(self):
+        return bool(self.data.more_url)
+
+    def has_footer(self):
+        return bool(self.data.footer)
