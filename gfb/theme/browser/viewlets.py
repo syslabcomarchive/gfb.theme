755587d00e67e5a5fd433abed95ff3eeca23f247
Alexander Pilz <pilz@syslab.com>
2009-02-17 06:49:22 +0000

    fixed the navbar problems (nearly done). Fixes for viewlets

--- gfb/theme/browser/viewlets.py
+++ gfb/theme/browser/viewlets.py
@@ -1,5 +1,7 @@
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.layout.viewlets.common import ViewletBase, PersonalBarViewlet, PathBarViewlet, GlobalSectionsViewlet, SearchBoxViewlet
+from zope.component import getMultiAdapter
+from Products.CMFCore.utils import getToolByName
 
 # Overwrite PersonalBarViewlet
 class PersonalBarViewletGFB(PersonalBarViewlet):
@@ -16,8 +18,42 @@ class SiteTitleViewlet(ViewletBase):
 class PathBarViewletGFB(PathBarViewlet):
     render = ViewPageTemplateFile('templates/path_bar.pt')
 
-class GlobalSectionsViewletGFB(GlobalSectionsViewlet):
+class GlobalSectionsViewletGFB(GlobalSectionsViewlet, SearchBoxViewlet):
     render = ViewPageTemplateFile('templates/sections.pt')
 
-class SearchBoxViewletGFB(SearchBoxViewlet):
-    render = ViewPageTemplateFile('templates/searchbox.pt')
+    def update(self):
+        # from viewletbase
+        self.portal_state = getMultiAdapter((self.context, self.request),
+                                            name=u'plone_portal_state')
+        self.site_url = self.portal_state.portal_url()
+
+        # from globalsections
+        context_state = getMultiAdapter((self.context, self.request),
+                                        name=u'plone_context_state')
+        actions = context_state.actions()
+        portal_tabs_view = getMultiAdapter((self.context, self.request),
+                                           name='portal_tabs_view')
+        self.portal_tabs = portal_tabs_view.topLevelTabs(actions=actions)
+
+        selectedTabs = self.context.restrictedTraverse('selectedTabs')
+        self.selected_tabs = selectedTabs('index_html',
+                                          self.context,
+                                          self.portal_tabs)
+        self.selected_portal_tab = self.selected_tabs['portal']
+
+
+        # from searchbox
+        props = getToolByName(self.context, 'portal_properties')
+        livesearch = props.site_properties.getProperty('enable_livesearch', False)
+        if livesearch:
+            self.search_input_id = "searchGadget"
+        else:
+            self.search_input_id = ""
+
+        folder = context_state.folder()
+        self.folder_path = '/'.join(folder.getPhysicalPath())
+        
+
+
+#class SearchBoxViewletGFB(SearchBoxViewlet):
+#    render = ViewPageTemplateFile('templates/searchbox.pt')
