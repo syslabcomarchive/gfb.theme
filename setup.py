ea03ff37b8106dc855d3c9b6bf740f98f8548807
Wolfgang Thomas <thomas@syslab.com>
2009-08-09 13:54:36 +0000

    prepare release 1.2.11

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.2.10'
+version = '1.2.11'
 
 long_description = (
     read('README.txt')
