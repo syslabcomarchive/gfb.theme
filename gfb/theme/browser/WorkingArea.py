1304daf4c2ef891b9e4fac10b2d6da1c1b1c57a0
Wolfgang Thomas <thomas@syslab.com>
2010-11-09 21:08:33 +0000

    added provider-overview & adapted workingarea manager view

--- gfb/theme/browser/WorkingArea.py
+++ gfb/theme/browser/WorkingArea.py
@@ -3,7 +3,10 @@ from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.CMFCore.utils import getToolByName
 from zope.component import getMultiAdapter
 from Products.LinguaPlone.browser.selector import TranslatableLanguageSelector
+from Products.LinguaPlone.interfaces import ITranslatable
+from plone.app.i18n.locales.browser.selector import LanguageSelector
 from Products.statusmessages.interfaces import IStatusMessage
+from Acquisition import aq_parent, aq_inner
 
 class WorkingArea(BrowserView, TranslatableLanguageSelector):
     """
@@ -25,6 +28,33 @@ class WorkingArea(BrowserView, TranslatableLanguageSelector):
     def __call__(self):
         return self.template()
 
+    def getTemplateName(self):
+        return "workingarea"
+
+    def languages(self):
+        results = LanguageSelector.languages(self)
+        translatable = ITranslatable(self.context, None)
+        if translatable is not None:
+            translations = translatable.getTranslations()
+        else:
+            translations = []
+
+        for data in results:
+            data['translated'] = data['code'] in translations
+            if data['translated']:
+                trans = translations[data['code']][0]
+                state = getMultiAdapter((trans, self.request),
+                        name='plone_context_state')
+                data['url'] = state.view_url() + '/' + self.getTemplateName() + '?set_language=' + data['code']
+            else:
+                state = getMultiAdapter((self.context, self.request),
+                        name='plone_context_state')
+                try:
+                    data['url'] = state.view_url() + '/' + self.getTemplateName() + '?set_language=' + data['code']
+                except AttributeError:
+                    data['url'] = self.context.absolute_url() + '/' + self.getTemplateName()  + '?set_language=' + data['code']
+
+        return results
 
     def setup(self):
         pm = getToolByName(self, 'portal_membership')
@@ -85,6 +115,7 @@ class WorkingArea(BrowserView, TranslatableLanguageSelector):
         provider = self.provider()
         return provider and provider.Title() or "n/a"
 
+
 class WorkingAreaManager(WorkingArea):
     """ Working Area that does not force the view to the currently logged in
         user's home folder, but displays the content of any user folder."""
@@ -93,14 +124,24 @@ class WorkingAreaManager(WorkingArea):
         super(WorkingAreaManager, self).__init__(context, request)
         self.error = ''
         self.userid = self.request.get('userid', '')
+        if not self.userid:
+            try:
+                self.userid = aq_parent(aq_inner(self.context)).getId() == 'Members' and \
+                    self.context.getId() or ''
+            except:
+                pass
+        pm = getToolByName(context, 'portal_membership')
         if self.userid:
-            pm = getToolByName(context, 'portal_membership')
             self.memberok = not not pm.getMemberById(self.userid)
             if not self.memberok:
                 self.error = 'No user found for %s' %self.userid
         else:
-            self.error = "You must provide a userid"
+            self.error = "You must provide a userid as parameter or call the view on a member folder"
             self.memberok = False
+
+
+    def getTemplateName(self):
+        return "workingarea-manager"
 
     def setup(self):
         if self.memberok:
@@ -125,3 +166,25 @@ class WorkingAreaManager(WorkingArea):
             status = IStatusMessage(self.request)
             status.addStatusMessage(self.error, type='error')
         return self.template()
+
+
+class ProviderOverview(BrowserView):
+
+    template = ViewPageTemplateFile('templates/provider_overview.pt')
+
+    def __call__(self):
+        return self.template()
+
+    def getMembers(self):
+        members = list()
+        portal = getToolByName(self.context, 'portal_url').getPortalObject()
+        mf = portal.restrictedTraverse('Members')
+        for id, ob in mf.objectItems('ATFolder'):
+            providers = ob.objectValues('Provider')
+            providers = [x for x in providers if x.isCanonical()]
+            provider = len(providers) and providers[0].Title() or 'n/a'
+            members.append(dict(id=id, name=ob.Title(), provider=provider,
+                url=ob.absolute_url()))
+
+        return members
+  
