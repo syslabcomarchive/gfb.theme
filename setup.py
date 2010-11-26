c79931503803ea5b1315b64f284e80a3eb47f34d
Wolfgang Thomas <thomas@syslab.com>
2010-11-26 11:26:41 +0000

    Back to development: 1.3.8

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.3.7'
+version = '1.3.8dev'
 
 long_description = (
     read('README.txt')
