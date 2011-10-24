eb1efd1f0984272e715a03415bbfe23bbf649bf6
Wolfgang Thomas <thomas@syslab.com>
2011-10-24 14:56:07 +0000

    we need special handling for the "remote provider", since only the first level comes from the ATVocabularyManager and the second level is compited from a catalog query

--- gfb/theme/browser/vdex.py
+++ gfb/theme/browser/vdex.py
@@ -1,6 +1,8 @@
 from collective.dynatree.utils import dict2dynatree
 from z3c.json.converter import JSONWriter
+from zope.component import getUtility
 from Products.CMFCore.utils import getToolByName
+from gfb.policy.interfaces import IVocabularyUtility
 
 class VDEXAsJson(object):
     def __init__(self, context, request):
@@ -8,8 +10,14 @@ class VDEXAsJson(object):
         self.context = context
 
     def __call__(self, vdex):
-        vocab_manager = getToolByName(self.context, 'portal_vocabularies')
-        vocab = vocab_manager[vdex]
-        vocab_dict = vocab.getVocabularyDict(self.context)
+        vocab_dict = self.getVocabulary(vdex)
         transformed_dict = dict2dynatree(vocab_dict, [], False, False)
         return JSONWriter().write(transformed_dict)
+
+    def getVocabulary(self, name):
+        if name != 'provider':
+            portal_vocabularies = getToolByName(self.context, 'portal_vocabularies')
+            return portal_vocabularies[name].getVocabularyDict(self.context)
+        else:
+            util = getUtility(IVocabularyUtility, name)
+            return util.getVocabularyDict()
