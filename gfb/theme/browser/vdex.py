7c0413cafd1c3c0e3a0dfe7e1c9624c80c352f7e
Wolfgang Thomas <thomas@syslab.com>
2011-10-24 16:03:11 +0000

    simplyfied / show keys for NACE

--- gfb/theme/browser/vdex.py
+++ gfb/theme/browser/vdex.py
@@ -4,20 +4,24 @@ from zope.component import getUtility
 from Products.CMFCore.utils import getToolByName
 from gfb.policy.interfaces import IVocabularyUtility
 
+
 class VDEXAsJson(object):
+
     def __init__(self, context, request):
         self.request = request
         self.context = context
 
     def __call__(self, vdex):
-        vocab_dict = self.getVocabulary(vdex)
-        transformed_dict = dict2dynatree(vocab_dict, [], False, False)
-        return JSONWriter().write(transformed_dict)
-
-    def getVocabulary(self, name):
-        if name != 'provider':
-            portal_vocabularies = getToolByName(self.context, 'portal_vocabularies')
-            return portal_vocabularies[name].getVocabularyDict(self.context)
+        if vdex != 'provider':
+            tool = getToolByName(self.context, 'portal_vocabularies')
+            vocab_dict = tool[vdex].getVocabularyDict(self.context)
         else:
-            util = getUtility(IVocabularyUtility, name)
-            return util.getVocabularyDict()
+            util = getUtility(IVocabularyUtility, vdex)
+            vocab_dict = util.getVocabularyDict()
+        if vdex == 'NACE':
+            showKey = True
+        else:
+            showKey = False
+
+        transformed_dict = dict2dynatree(vocab_dict, [], False, showKey)
+        return JSONWriter().write(transformed_dict)
