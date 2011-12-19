7115eca598d6ac68a444d055ed875df217bc0634
Wolfgang Thomas <thomas@syslab.com>
2011-12-19 16:41:23 +0000

    re-enabled news-lsiting

--- gfb/theme/browser/news_listing.py
+++ gfb/theme/browser/news_listing.py
@@ -1,25 +1,37 @@
-from osha.theme.browser.oshnews_view import OSHNewsLocalView
+# -*- coding: utf-8 -*-
 from plone.memoize import instance
 import Acquisition
 from Products.AdvancedQuery import Or, Eq, And, In, Le
+from Products.CMFPlone.PloneBatch import Batch
 from Products.CMFCore.utils import getToolByName
 from DateTime import DateTime
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.Five.browser import BrowserView
+from Products.ATContentTypes.interface import IATTopic
 
-class LocalNewsListing(OSHNewsLocalView):
-    
-    template = ViewPageTemplateFile('templates/oshnews_view.pt')
-    
-    @instance.memoize
+class LocalNewsListing(BrowserView):
+
+    def __call__(self):
+        return self.index()
+
+    def getName(self):
+        return self.__name__
+
+    def Title(self):
+        context = Acquisition.aq_inner(self.context)
+        if IATTopic.providedBy(context):
+            return context.Title()
+        return _(u"heading_newsboard_latest_news")
+
+    #@instance.memoize
     def getResults(self):
         context = Acquisition.aq_inner(self.context)
-        if context.portal_type == 'Topic':
+        if IATTopic.providedBy(context):
             results = context.queryCatalog()
         else:
             catalog = getToolByName(context, 'portal_catalog')
             if hasattr(catalog, 'getZCatalog'):
                 catalog = catalog.getZCatalog()
-            
+
             now = DateTime()
             queryA = Eq('portal_type', 'News Item')
             queryBoth = In('review_state', 'published') & Eq('path', '/'.join(context.getPhysicalPath())) \
@@ -29,3 +41,18 @@ class LocalNewsListing(OSHNewsLocalView):
             results = catalog.evalAdvancedQuery(query, (('Date', 'desc'),) )
 
         return results
+
+    def queryCatalog(self, b_size=20):
+        results = self.getResults()
+        b_start = self.request.get('b_start', 0)
+        batch = Batch(results, b_size, int(b_start), orphan=0)
+        return batch
+
+    def getBodyText(self):
+        """ returns body text of collection  if present """
+        context = Acquisition.aq_base(Acquisition.aq_inner(self.context))
+        text = getattr(context, 'getText', None) and context.getText() or ''
+        return text
+
+    def showLinkToNewsItem(self):
+        return self.context.getProperty('show_link_to_news_item', True)
