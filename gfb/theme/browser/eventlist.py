50a721401b4b649ad92001ad62ad7ecc8ea78521
Wolfgang Thomas <thomas@syslab.com>
2012-08-06 15:12:01 +0000

    removed stale imports

--- gfb/theme/browser/eventlist.py
+++ gfb/theme/browser/eventlist.py
@@ -1,24 +1,8 @@
 # -*- coding: utf-8 -*-
 """The List Events view."""
 
-from datetime import date
-from iwwb.eventlist import _
-from iwwb.eventlist.interfaces import IIWWBSearcher
-from iwwb.eventlist.interfaces import IListEventsForm
-from iwwb.eventlist.interfaces import IWWB_SEARCHABLE_FIELDS
-from iwwb.eventlist.browser.eventlist import ListEventsForm, ListEventsView
-from plone.z3cform.layout import FormWrapper
+from iwwb.eventlist.browser.eventlist import ListEventsView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from Products.statusmessages.interfaces import IStatusMessage
-from z3c.form import button
-from z3c.form import field
-from z3c.form import form
-from zope.component import getUtility
-from zope.schema.interfaces import IVocabularyFactory
-
-import logging
-
-logger = logging.getLogger('iwwb.eventlist')
 
 
 class GFBListEventsView(ListEventsView):
