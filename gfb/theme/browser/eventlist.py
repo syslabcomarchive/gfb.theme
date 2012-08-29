6b88a5549e83cef8e335d2127320eea5675eb481
Wolfgang Thomas <thomas@syslab.com>
2012-08-29 15:38:20 +0000

    ok, so we DO need to customize the eventlist view to get our local design changes in

---
+++ gfb/theme/browser/eventlist.py
@@ -0,0 +1,7 @@
+from iwwb.eventlist.browser.eventlist import ListEventsView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+
+class GFBListEventsView(ListEventsView):
+    """A BrowserView to display the ListEventsForm along with its results."""
+    index = ViewPageTemplateFile('templates/eventlist.pt')
