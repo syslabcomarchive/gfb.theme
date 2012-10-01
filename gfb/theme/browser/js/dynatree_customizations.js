23650d7bad33bcfca24e48bd5084e7bfc733b228
Wolfgang Thomas <thomas@syslab.com>
2012-10-01 16:46:14 +0000

    added Patrick's magic to get Risk Factors on the evenlist form

--- gfb/theme/browser/js/dynatree_customizations.js
+++ gfb/theme/browser/js/dynatree_customizations.js
@@ -20,6 +20,7 @@ var CustomHiddenForm = Backbone.View.extend({
     input_template: _.template('<input type="hidden" name="{{ name }}:list" value="{{ value }}" />')
 });
 
+
 jq(document).ready(function(){
     jq(".dynatree-custom").each(function() {
         var jqthis = jq(this);
@@ -28,15 +29,15 @@ jq(document).ready(function(){
                                                        model: datamodel});
     });
     jq(".closed_widget").hide();
-    
-    function runEffect(fname) {        
-        // get effect type from 
+
+    function runEffect(fname) {
+        // get effect type from
         var selectedEffect = "blind";
         // most effect types need no options passed by default
         var options = {};
         jq( "#" + fname ).toggle( selectedEffect, options, 500 );
     };
-    
+
     // set effect from select menu value
     jq( ".toggleButton" ).click(function() {
         var jqthis = jq(this);
@@ -46,4 +47,34 @@ jq(document).ready(function(){
         runEffect(fname);
         return false;
     });
+
+javascript: if (!String.prototype.trim) {
+    String.prototype.trim = function () {
+        return this.replace(/^\s+|\s+$/g, '');
+    };
+}
+
+
+function dynatree_to_input(input, dynatree) {
+    var model = dynatree.data("collective.dynatree");
+    model.bind("change:selected", function (model) {
+        var old_keys = model.previous('selected'),
+            new_keys = model.changedAttributes().selected,
+            new_input = input.val().trim();
+        _.each(_.difference(old_keys, new_keys), function (to_remove) {
+            var to_remove_title = model.getDataFor(to_remove).title;
+            new_input = new_input.replace(to_remove_title, "");
+        });
+        _.each(_.difference(new_keys, old_keys), function (to_add) {
+            var to_add_title = model.getDataFor(to_add).title;
+            new_input += " " + to_add_title;
+        });
+        input.val(new_input.trim());
+    });
+}
+
+var custom_popup = jq(".dynatree-custom-popup");
+if (custom_popup.size() > 0) {
+    dynatree_to_input(jq("#form-widgets-query"), jq(custom_popup[0]));
+}
 });
