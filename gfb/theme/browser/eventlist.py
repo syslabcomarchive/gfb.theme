c1c56c46f7166714350383e147e708fc00a6a0fa
Wolfgang Thomas <thomas@syslab.com>
2012-10-13 09:18:19 +0000

    serve values of preselection vocabulary

--- gfb/theme/browser/eventlist.py
+++ gfb/theme/browser/eventlist.py
@@ -2,6 +2,7 @@ from iwwb.eventlist.browser.eventlist import ListEventsView, ListEventsForm
 from plone.z3cform.templates import subform_factory
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.Five.browser.pagetemplatefile import BoundPageTemplate
+from Products.CMFCore.utils import getToolByName
 
 
 class GFBListEventsForm(ListEventsForm):
@@ -18,3 +19,12 @@ class GFBListEventsView(ListEventsView):
     """A BrowserView to display the ListEventsForm along with its results."""
     index = ViewPageTemplateFile('templates/eventlist.pt')
     form = GFBListEventsForm
+
+    def get_preselection(self):
+        tool = getToolByName(self.context, 'portal_vocabularies')
+        vocab_name = 'eventlist_preselection'
+        if vocab_name not in tool:
+            return list()
+        vocab_dict = tool[vocab_name].getVocabularyDict(
+            self.context)
+        return [x[0] for x in vocab_dict.values()]
