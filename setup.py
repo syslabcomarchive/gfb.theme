2a3a8c045595ac17383ae03bc0806512186b7b6d
Wolfgang Thomas <thomas@syslab.com>
2012-10-31 15:48:25 +0000

    Added parameter for correct sorting by date

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.4.14'
+version = '1.4.15.dev0'
 
 long_description = (
     read('README.txt')
