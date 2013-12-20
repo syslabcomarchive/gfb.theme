b7aa1ca7c62e979d25d67e358af02d80bf62cdc3
Cillian de Roiste <cillianderoiste@gmail.com>
2013-12-20 09:03:21 +0000

    Refs #9044 Search results: fix the adapter which sets the title on portal_type icons

--- gfb/theme/browser/icon.py
+++ gfb/theme/browser/icon.py
@@ -1,42 +1,17 @@
-from zope.interface import implements
 from Acquisition import aq_inner
-from Products.CMFCore.utils  import getToolByName
-from Products.CMFPlone.utils import safe_unicode
-from plone.app.layout.icons.interfaces import IContentIcon
-from plone.app.layout.icons.icons import BaseIcon
+from Products.CMFCore.utils import getToolByName
 from plone.app.layout.icons.icons import CatalogBrainContentIcon
-from plone.app.layout.icons.icons import DefaultContentIcon
-from zope.component import getMultiAdapter
 
 
-class ContentIconWithTitle(DefaultContentIcon):
-    """ #9044 set the icon title to the portal_type
-    """
-    implements(IContentIcon)
+class CatalogBrainContentIconWithTitle(CatalogBrainContentIcon):
 
     @property
     def title(self):
         context = aq_inner(self.context)
         tt = getToolByName(context, 'portal_types')
-        fti = tt.get(self.obj['portal_type'])
+        portal_type = self.brain.get("portal_type")
+        fti = tt.get(portal_type)
         if fti is not None:
             return fti.Title()
         else:
-            return self.obj['portal_type']
-
-    @property
-    def description(self):
-        if self.obj is None:
-            return None
-        return safe_unicode(self.obj.Title)
-
-    @property
-    def url(self):
-        path = self.obj.getIcon
-        if not path:
-            return path
-
-        portal_state_view = getMultiAdapter(
-            (self.context, self.request), name=u'plone_portal_state')
-        portal_url = portal_state_view.portal_url()
-        return "%s/%s" % (portal_url, path)
+            return portal_type
