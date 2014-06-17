60e2b37151171f0eb1ca697137d2fcdfe7955ed2
Cillian de Roiste <cillianderoiste@gmail.com>
2014-06-17 14:41:15 +0000

    Refs #10139 translate fti icons used in solr search results

--- gfb/theme/browser/icon.py
+++ gfb/theme/browser/icon.py
@@ -11,7 +11,15 @@ class CatalogBrainContentIconWithTitle(CatalogBrainContentIcon):
         tt = getToolByName(context, 'portal_types')
         portal_type = self.brain.get("portal_type")
         fti = tt.get(portal_type)
+        lt = getToolByName(self.context, 'portal_languages')
+        lang = lt.getPreferredLanguage()
+
         if fti is not None:
-            return fti.Title()
+            return context.translate(
+                domain="plone",
+                msgid=fti.Title(),
+                default=fti.Title(),
+                target_language=lang,
+            )
         else:
             return portal_type
