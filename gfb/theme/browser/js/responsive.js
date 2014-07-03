b8689b94dc63720138cc4e6bdef5d173f16adeff
Cillian de Roiste <cillianderoiste@gmail.com>
2014-07-03 16:43:39 +0000

    Refs #6279 Fix Firefox ESR 17 compatibility with the responsive theme

--- gfb/theme/browser/js/responsive.js
+++ gfb/theme/browser/js/responsive.js
@@ -1,8 +1,11 @@
 $(function() {
     if ($(window).width() < 1000) {
-        var navTreeHeight = $("ul.navTreeLevel0").height();
-        if (! navTreeHeight) { navTreeHeight = 0 };
-        var footerActions = $("ul#portal-siteactions")[1];
-        $(footerActions).css({"padding-bottom": navTreeHeight + "px"});
+      var navTree = $("ul.navTreeLevel0");
+      var navTreeHeight = $("ul.navTreeLevel0").height() || 0;
+      var globalnavTop = $('#globalnav-group').position()["top"];
+      globalNavHeight = $('#globalnav-group').height();
+      totalNavHeight = globalNavHeight + navTreeHeight;
+      $("#visual-portal-wrapper").css({"padding-bottom": totalNavHeight + "px"});
+      navTree.css({"bottom": globalNavHeight + "px", "visibility": "visible" });
     }
 });
