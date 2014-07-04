974877e9a107331c3bc71d2d6d7a3cea5c865d09
Cillian de Roiste <cillianderoiste@gmail.com>
2014-07-04 13:38:33 +0000

    Responsive.js, remove unused code

--- gfb/theme/browser/js/responsive.js
+++ gfb/theme/browser/js/responsive.js
@@ -2,7 +2,6 @@ $(function() {
     if ($(window).width() < 1000) {
       var navTree = $("ul.navTreeLevel0");
       var navTreeHeight = $("ul.navTreeLevel0").height() || 0;
-      var globalnavTop = $('#globalnav-group').position()["top"];
       globalNavHeight = $('#globalnav-group').height();
       totalNavHeight = globalNavHeight + navTreeHeight;
       $("#visual-portal-wrapper").css({"padding-bottom": totalNavHeight + "px"});
