8371838a794cc211da9fee78a6ee1408af83149c
Wolfgang Thomas <thomas@syslab.com>
2014-07-24 17:35:10 +0200

    also show search suggestions on main search form #10451

--- gfb/theme/browser/js/search-suggestions.js
+++ gfb/theme/browser/js/search-suggestions.js
@@ -1,14 +1,8 @@
 $(function() {
-    var searchInput = $(".LSBox input[name='SearchableText']");
-    $(searchInput).attr("autocomplete", "off");
 
-    searchInput.bind( "paste keyup", function ( event ) {
-        var term = event.target.value;
 
-        $.getJSON("suggest-terms?term="+term, function( data ) {
-            $("#suggestion-area").remove();
-            $(event.target).parent().append("<div id='suggestion-area'/>");
-
+    var search_suggestion = {
+        _fillSuggestionArea: function(event, data) {
             /* Sometimes we get back a list of strings, instead of
                objects with useful values, just ignore them */
             if (data.length > 0 && data[0].constructor == Object) {
@@ -19,19 +13,53 @@ $(function() {
                 $(data).each( function (index, value) {
                     if ( value.constructor == Object ) {
                         suggestions.append("<li>"+value.value+"</li>");
-                    };
+                    }
                 });
 
                 suggestions.find("li").click(function() {
                     $(event.target).val($(this).text());
                     $("#suggestion-area").remove();
                 });
-            };
+            }
+        }
+    };
+
+    /* Binding for the quick search box */
+    var searchInput = $(".LSBox input[name='SearchableText']");
+    $(searchInput).attr("autocomplete", "off");
+
+    searchInput.bind( "paste keyup", function ( event ) {
+        var term = event.target.value;
+
+        $.getJSON("suggest-terms?term="+term, function( data ) {
+            $("#suggestion-area").remove();
+            $(event.target).parent().append("<div id='suggestion-area'/>");
+
+            search_suggestion._fillSuggestionArea(event, data);
         });
     });
-
     searchInput.blur(function() {
         // Allow time for a click event to be fired from the suggestion-area
         setTimeout("$('#suggestion-area').remove()", 200);
     });
+
+    /* Binding for the main search field */
+    var searchInputMain = $(".LSBoxRALinks input[name='SearchableText']");
+    $(searchInputMain).attr("autocomplete", "off");
+
+    searchInputMain.bind( "paste keyup", function ( event ) {
+        var term = event.target.value;
+
+        $.getJSON("suggest-terms?term="+term, function( data ) {
+            $("#suggestion-area").remove();
+            $(event.target).after("<div id='suggestion-area'/>");
+
+            search_suggestion._fillSuggestionArea(event, data);
+        });
+    });
+    searchInputMain.blur(function() {
+        // Allow time for a click event to be fired from the suggestion-area
+        setTimeout("$('#suggestion-area').remove()", 200);
+    });
+
 });
