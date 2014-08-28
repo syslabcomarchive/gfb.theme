a15afd09cf3bc845026c3e50528bcb94324b8c97
Wolfgang Thomas <thomas@syslab.com>
2014-08-28 14:05:11 +0200

    Built a (rather crudely hardcoded) browser view for displaying the sign language video, which MUST be present in mp4 and webw with id 'gebaerdensprache' in the /de language root #10606

---
+++ gfb/theme/browser/signlanguage.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from Products.Five.browser import BrowserView
+
+
+class SignLanguage(BrowserView):
+
+    pass
+
+    # I thought we would do clever stuff in here but currently we simply
+    # use a PageTemplate.
