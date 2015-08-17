531541e3fc8d5e99f0f7136908c6005abc251f90
Wolfgang Thomas <thomas@syslab.com>
2015-08-17 13:52:43 +0200

    Preparing release 1.4.53

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.4.53.dev0'
+version = '1.4.53'
 
 long_description = (
     read('README.txt')
